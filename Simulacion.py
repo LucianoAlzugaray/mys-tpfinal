1b603eee8b954a1d82bb5df40a8f7477ac3aeadb
LuluAllende19 <abigail_lucii@hotmail.com>
2020-06-20 19:59:26 -0300

    Agregado el experimento a simulacion

--- Simulacion.py
+++ Simulacion.py
@@ -9,12 +9,13 @@ class Simulacion:
 
     def correr_simulacion(self):
         #Correr simulacion
-        #por cada dia, generar un nuevo objeto dia y correrlo
-        for dia in range(self.dias_a_simular):
-            nuevo_dia = Dia(minutos_maximo)
-            nuevo_dia.correr()
-        #  guarda el dia en una lista de dias corridos.
-        self.dias_corridos.append(nuevo_dia)
+        for experimento in range(self.experimentos):
+            #por cada dia, generar un nuevo objeto dia y correrlo
+            for dia in range(self.dias_a_simular):
+                nuevo_dia = Dia(minutos_maximo)
+                nuevo_dia.correr()
+            #  guarda el dia en una lista de dias corridos.
+            self.dias_corridos.append(nuevo_dia)
 
     def obtener_datos(self):
         # Obtener datos finales
